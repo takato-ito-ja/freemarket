9cfa371814a8cfcac6f129eff1e776f47ce7e545
takato-ito-ja <takato0511@gmail.com>
2020-02-06 15:41:14 +0900

    データベースの修正

--- db/migrate/20200131095254_create_products.rb
+++ db/migrate/20200131095254_create_products.rb
@@ -1,24 +1,22 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-      t.references :seller_id,               null: false, foreign_key: { to_table: :users }
-      t.string     :name,                    null: false
-      t.integer    :price,                   null: false
-      t.text       :description,             null: false
-      t.integer    :condition,               null: false
-      t.integer    :postage_burden,          null: false
-      t.integer    :scheduled_sending_date,  null: false
-      t.integer    :size,                    null: false
-      t.integer    :status,                  null: false
-      t.references :buyer_id,                foreign_key: { to_table: :users }
-      t.timestamps :buyed_time
-      t.integer    :payment_method,          null: false
-      t.integer    :payment_status,          null: false
-      t.integer    :sending_status,          null: false
-      t.integer    :recieving_status,        null: false
-      t.integer    :sending_methods,         null: false
+      t.references :seller,               foreign_key: { to_table: :users }
+      t.references :buyer,                foreign_key: { to_table: :users }
+      t.string     :name   
+      t.integer    :price                  
+      t.text       :description            
+      t.integer    :condition              
+      t.integer    :postage_burden         
+      t.integer    :scheduled_sending_date 
+      t.integer    :size                   
+      t.integer    :status                 
+      t.integer    :payment_status         
+      t.integer    :sending_status         
+      t.integer    :recieving_status       
+      t.integer    :sending_method         
 
-      # t.timestamps 
+      t.timestamps 
 
     end
   end
