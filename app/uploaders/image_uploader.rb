bdf56e78d0be27ff93f6e4380574d352b0067330
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-02-07 17:49:51 +0900

    S3の保存先の条件分岐

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fit: [100, 100]
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # storage :fog
+  
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
