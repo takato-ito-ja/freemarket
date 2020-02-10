77927d127ff785b0f2a6a956907ecebd6df43477
takato-ito-ja <takato0511@gmail.com>
2020-02-10 12:16:01 +0900

    console.logの削除

--- app/assets/javascripts/product-show.js
+++ app/assets/javascripts/product-show.js
@@ -1,7 +1,5 @@
 $(function () {
-  console.log("test");
   $('.image-list').click(function () {
-    console.log("test");
     var $src = $(this).attr('src');
     $('.main-image').attr('src', $src);
   });
