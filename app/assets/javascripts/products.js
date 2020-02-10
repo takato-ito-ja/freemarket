9dc939e5162578d71a65cab1d5d0bf34912696bb
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-02-10 13:07:08 +0900

    seed 追加

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -4,14 +4,6 @@
 
 $(function () {
 
-  var buildPrompt = `<option value>---</option>`
-  var buildHtmlOption = function (cat) {
-    var option = `<option value ="${cat.id}">${cat.name}</option>`
-    return option
-  }
-
-
-  
   $('.sell__image-select__ul-text').on('change', 'input[type="file"]', function (e) {
     var id = $(this).data('id'),
       file = e.target.files[0],
@@ -52,61 +44,4 @@ $(function () {
     $(this).parent().parent().css('opacity', '0');
     return false;
   });
-
-
-
-
-
-  $('#parent').change(function () {
-    var parent_id = $(this).val();
-    $.ajax({
-      type: 'GET',
-      url: '/api/sell/child',
-      data: { id: parent_id },
-      dataType: 'json'
-    })
-      .done(function (categories) {
-        $('.child').css('display', 'block');
-        $('#child').empty();
-        $('.grand_child').css('display', 'none');
-        $('#child').append(buildPrompt);
-
-        categories.forEach(function (cat) {
-          var html_option = buildHtmlOption(cat);
-          $('#child').append(html_option);
-        });
-      })
-      .fail(function () {
-      });
-  });
-
-  $(this).on("change", "#child", function () {
-    var child_id = $("#child").val();
-    $.ajax({
-      type: 'GET',
-      url: '/api/sell/grand_child',
-      data: { id: child_id },
-      dataType: 'json'
-    })
-      .done(function (categories) {
-        $('.grand_child').css('display', 'block');
-        $('#grand_child').empty();
-        $('#grand_child').append(buildPrompt);
-        categories.forEach(function (cat) {
-          var html_option = buildHtmlOption(cat);
-          $('#grand_child').append(html_option);
-        });
-      })
-      .fail(function () {
-      });
-  });
-
-  $("#product_price").on("keyup", function () {
-    var price = $(this).val();
-    var fee = Math.floor(price * 0.1);
-    $(".sell__about__right__wrap__fee-right").text("¥ " + fee);
-    var profit = price - fee
-    $(".sell__about__right__wrap__profit-right").text("¥ " + profit);
-  });
-
 });
