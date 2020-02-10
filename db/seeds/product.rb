5126400b8ac3bfd3882c2e0eb72a069d9611530d
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-02-10 15:12:45 +0900

    seed 追加

---
+++ db/seeds/product.rb
@@ -0,0 +1,3 @@
+Product.create(name: "洋服", price: "5000円", description: "新品同様です", condition:1, postage_burden: 1, scheduled_sending_date: 1, size: 0, status:0, payment_status:0, sending_status:1, recieving_status:1, sending_method:1, seller_id:1, category_id:2, area_id:1)
+Product.create(name: "ズボン", price: "4000円", description: "中古です", condition:1, postage_burden:1, scheduled_sending_date: 2, size: 0, status:0, payment_status:0, sending_status:1, recieving_status:1, sending_method:1, seller_id:1, category_id:2, area_id:1)
+Product.create(name: "帽子", price: "10000円", description: "超高級な素材です", condition:2, postage_burden: 1, scheduled_sending_date: 2, size: 2, status:2, payment_status:2, sending_status:2, recieving_status:2, sending_method:2, seller_id:2, category_id:2, area_id:1)
