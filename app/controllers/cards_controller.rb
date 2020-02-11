3d8be2e34167fc95d581b534e476dc9f6ce4b733
rikuya0048 <rikuya0048@gmail.com>
2020-02-11 14:28:55 +0900

    find_byをfindに変更
    購入確認画面へのパスも設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,7 +3,7 @@ class CardsController < ApplicationController
 def show
   # @product = Product.find_by(id: params[:id])
 
-  @product = Product.find_by(id: params[:id])
+  @product = Product.find(params[:id])
   @seller = User.find_by(id: @product.seller_id)
 end
 
