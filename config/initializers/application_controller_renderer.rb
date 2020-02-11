9c70bdcade66a3781145defbdf972edd6639fc87
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-02-11 19:34:35 +0900

    いろいろ微修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,20 @@
-# Be sure to restart your server when you modify this file.
 
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      config.i18n.default_locale = :ja  
+      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s
+    end
+    
+  end
