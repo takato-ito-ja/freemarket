bae42decdc484174877aabbb93828e0d4d868315
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-02-11 12:09:30 +0900

    アソシエーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -26,9 +26,6 @@ class Product < ApplicationRecord
 
   enum size: [:"XS以下", :"S", :"M", :"L", :"XL",:"XL以上",:"FREE_SIZE"]
   enum condition: [ :"新品、未使用", :"未使用に近い", :"目立った傷や汚れなし",:"やや傷や汚れあり",:"傷や汚れあり", :"全体的に状態が悪い"]
-  # enum postage_burden: { "送料込み(出品者負担)": 1, "着払い(購入者負担)": 2}
-  # enum sending_method: { "未定": 1, "らくらくメルカリ便": 2, "ゆうメール": 3, "レターパック": 4, "普通郵便(定形、定形外)": 5, "クロネコヤマト": 6, "ゆうパック": 7, "クリックポスト": 8, "ゆうパケット": 9}
-  # enum scheduled_sending_date: { "1~2日で発送": 1, "2~3日で発送": 2, "4~7日で発送": 3}
   enum postage_burden: [:"送料込み(出品者負担)" ,:"着払い(購入者負担)"]
   enum sending_method:[:"未定",:"らくらくメルカリ便", :"ゆうメール"]
   enum scheduled_sending_date: [:"1~2日で発送", :"2~3日で発送",:"4~7日で発送"]
