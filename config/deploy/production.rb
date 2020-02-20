b8cd4566ac4c3b10055993974fad011e8c8b98fb
takato-ito-ja <takato0511@gmail.com>
2020-02-20 15:37:27 +0900

    自動デプロイの準備

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.178.148.118', user: 'ec2-user', roles: %w{app db web}
+server '3.113.57.227', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
