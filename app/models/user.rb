9713340eb89f91b061fc6981076d7d70b4458ced
takato-ito-ja <takato0511@gmail.com>
2020-02-20 20:16:55 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
     validates :password, length: { in: 6..15, message: '6〜15文字で記入してください'}, presence: true
     validates :last_name, presence: true
     validates :first_name, presence: true
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ , message: "は全角カタカナで登録してください"}
+    validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/ , message: "は全角カタカナで登録してください"}
     validates :phone_number, length: { in: 10..11, message: 'は10,11文字で記入してください'}, presence: true
     validates_uniqueness_of :phone_number, message: 'は既に登録されています'
   
