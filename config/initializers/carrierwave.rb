33d1b5f398e6e471dbc1b3b940ae4381ec93ab6d
takato-ito-ja <takato0511@gmail.com>
2020-02-20 14:58:46 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,8 +12,8 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'frima67e'  #S3のバケット名
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/frima67e'
+    config.fog_directory  = 'freemarket1309'  #S3のバケット名
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket1309'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
